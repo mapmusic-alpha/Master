cd7f8b23d3fd42f49e166d418b6f20638f02654f
Kiran Boyle <anthony.kiran.boyle@gmail.com>
2021-09-15 09:56:37 +0200

    fixed some issues after the merge

--- routes/auth.js
+++ routes/auth.js
@@ -24,50 +24,50 @@ router.post('/log-in', passport.authenticate('local', {
 
 }))
 
-// router.post("/sign-up", (req, res, next) => {
-//     const username = req.body.username
-//     const password = req.body.username
-//     const email = req.body.email
-//     const emailRepetition = req.body.emailRepetition
-//     console.log(req.body)
-//     if (password.length < 5) {
-//         res.render("sign-up", { message: "Your password should be longer than 5 characters" })
-//         return;
-//     }
-//     if (username.length === 0) {
-//         res.render("sign-up", { message: "Your username cannot be empty" })
-//         return;
-//     }
-//     if (email !== emailRepetition) {
-//         res.render("sign-up", { message: "Your Email address is incorrect" })
-//         return;
-//     }
+router.post("/sign-up", (req, res, next) => {
+    const username = req.body.username
+    const password = req.body.username
+    const email = req.body.email
+    const emailRepetition = req.body.emailRepetition
+    console.log(req.body)
+    if (password.length < 5) {
+        res.render("sign-up", { message: "Your password should be longer than 5 characters" })
+        return;
+    }
+    if (username.length === 0) {
+        res.render("sign-up", { message: "Your username cannot be empty" })
+        return;
+    }
+    if (email !== emailRepetition) {
+        res.render("sign-up", { message: "Your Email address is incorrect" })
+        return;
+    }
 
-//     User.findOne({ username: username })
-//         .then(userFromDB => {
-//             if (userFromDB !== null) {
-//                 console.log('did not create user')
-//                 res.render('sign-up', { message: "This username is unavailable" })
-//                 return
+    User.findOne({ username: username })
+        .then(userFromDB => {
+            if (userFromDB !== null) {
+                console.log('did not create user')
+                res.render('sign-up', { message: "This username is unavailable" })
+                return
 
-//             } else {
+            } else {
 
-//                 const salt = bcrypt.genSaltSync()
-//                 const hash = bcrypt.hashSync(password, salt)
+                const salt = bcrypt.genSaltSync()
+                const hash = bcrypt.hashSync(password, salt)
 
-//                 User.create({ username: username, password: hash, email: email })
-//                     .then(createdUser => {
+                User.create({ username: username, password: hash, email: email })
+                    .then(createdUser => {
 
-//                         console.log(createdUser)
-//                         res.redirect('log-in')
+                        console.log(createdUser)
+                        res.redirect('log-in')
 
-//                     })
-//                     .catch(err => {
-//                         next(err)
-//                     })
-//             }
-//         })
-// })
+                    })
+                    .catch(err => {
+                        next(err)
+                    })
+            }
+        })
+})
 
 router.get('/logout', (req, res, next) => {
 
